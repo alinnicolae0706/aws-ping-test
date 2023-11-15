e1f1082cfc6697f9c3a1ab35141109137ea404a8
Alin Nicolae <alin.gabriel.nicolae@accenture.com>
2023-11-15 23:53:36 +0200

    fix version constraint warning

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
-  version = "~> 3.0"
   region  = "eu-central-1"
 }
 
